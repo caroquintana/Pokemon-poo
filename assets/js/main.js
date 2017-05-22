1a00afc3ccbf8e17c2ea0e2d545bc0c655da7b72
caroquintana <caro.quintana.n@gmail.com>
2017-05-21 22:04:56 -0400

    avance js

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,7 @@ function Pokemon(nombre, color, poderDeAtaque){
 	this.vida = 100;
 
 	this.mostrarPokemon = function(){
-		return("Hola, soy: " + this.nombre + " y soy de color: " + this.color)
+		return("Hola, soy: " + this.nombre + " y soy de color: " + this.color);
 	}
 
 	this.aumentarAmistad = function(valor){
@@ -19,10 +19,41 @@ function Pokemon(nombre, color, poderDeAtaque){
 
 }
 
+var Pikachu = new Pokemon("Pikachu", "amarillo", 100);
+var Charmander = new Pokemon("Charmander", "rojo", 20);
+var Venusaur = new Pokemon("Venusaur", "verde", 82);
+var Nidoqueen = new Pokemon("Nidoqueen", "azul", 31);
 
-const Pikachu = new Pokemon("Pikachu", "amarillo", 100)
-const Charmander = new Pokemon("Charmander", "rojo", 20)
+//Pikachu.atacar(Charmander);
 
-Pikachu.atacar(Charmander);
+//console.log(Charmander.vida);
 
-console.log(Charmander.vida);
+function pelea(nombre, color, poderDeAtaque){
+ 	var pokemon1 = document.getElementById("pokemon1").value; //valor select 1
+ 	var pokemon2 = document.getElementById("pokemon2").value; //valor select 2
+	var casilla_resultado = document.getElementById("resultado"); //donde se mostrará el resultado
+
+	var poke1 = new Pokemon(pokemon1 + color + poderDeAtaque);
+	var poke2 = new Pokemon(pokemon2 + color + poderDeAtaque);
+
+casilla_resultado.innerHTML = "¡" + poke1.nombre + " atacó a " + poke2.nombre + " y " + poke2.nombre + " tiene " + poke2.vida + " de vida restante."+ "!";
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
