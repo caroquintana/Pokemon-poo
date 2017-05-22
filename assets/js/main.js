f10b48d2eb8d4967de5aa51c818d443a1bac8115
caroquintana <caro.quintana.n@gmail.com>
2017-05-21 23:31:36 -0400

    fin

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,46 +1,72 @@
-function Pokemon(nombre, color, poderDeAtaque){
-	this.nombre = nombre;
-	this.color = color;
-	this.poderDeAtaque = poderDeAtaque;			
-	this.nivelDeAmistad = 0;
-	this.vida = 100;
+// function Pokemon (nombre, color, poderDeAtaque){
+// 	this.nombre = nombre;
+// 	this.color = color;
+	
+// 	this.nivelDeAmistad = 0;
+	
+// 	this.poderDeAtaque = poderDeAtaque;			
+// 	this.vida = 100;
+
+// 	this.mostrarPokemon = function(){
+// 		return("Hola, soy: " + this.nombre + " y soy de color: " + this.color);
+// 	}
+
+// 	this.aumentarAmistad = function(valor){
+// 		this.nivelDeAmistad = this.nivelDeAmistad + valor;
+// 	}
+
+// 	this.atacar = function(pokemon){
+// 		pokemon.vida = pokemon.vida - this.poderDeAtaque;
+// 	}
+
+// }
+
+function Pokemon(nombre,color,poderDeAtaque){
+	this.nombre = nombre
+	this.color = color
+
+	this.nivelDeAmistad = 0
+
+	this.vida = 100
+	this.poderDeAtaque = poderDeAtaque
 
 	this.mostrarPokemon = function(){
-		return("Hola, soy: " + this.nombre + " y soy de color: " + this.color);
+		return('Hola, soy: '+ this.nombre + ' y soy de color: ' + this.color)
 	}
-
 	this.aumentarAmistad = function(valor){
-		this.nivelDeAmistad = this.nivelDeAmistad + valor;
+		this.nivelDeAmistad  = this.nivelDeAmistad + valor
+		return this.nivelDeAmistad
 	}
-
 	this.atacar = function(pokemon){
 		pokemon.vida = pokemon.vida - this.poderDeAtaque;
+		return pokemon.vida
 	}
-
 }
 
-var Pikachu = new Pokemon("Pikachu", "amarillo", 100);
-var Charmander = new Pokemon("Charmander", "rojo", 20);
-var Venusaur = new Pokemon("Venusaur", "verde", 82);
-var Nidoqueen = new Pokemon("Nidoqueen", "azul", 31);
+const Pikachu = new Pokemon('Pikachu','amarillo',100)
+const Charmander = new Pokemon('Charmander','rojo',20)
+const Venusaur = new Pokemon('Venusaur','verde',82)
+const Nidoqueen = new Pokemon('Nidoqueen','azul',20)
+
 
 //Pikachu.atacar(Charmander);
 
 //console.log(Charmander.vida);
 
 function pelea(nombre, color, poderDeAtaque){
- 	var pokemon1 = document.getElementById("pokemon1").value; //valor select 1
- 	var pokemon2 = document.getElementById("pokemon2").value; //valor select 2
-	var casilla_resultado = document.getElementById("resultado"); //donde se mostrará el resultado
+ 	var pokemon1 = document.getElementById('pokemon1').value; //valor select 1
+ 	var pokemon2 = document.getElementById('pokemon2').value; //valor select 2
+	var casilla_resultado = document.getElementById('resultado'); //donde se mostrará el resultado
 
-	var poke1 = new Pokemon(pokemon1 + color + poderDeAtaque);
-	var poke2 = new Pokemon(pokemon2 + color + poderDeAtaque);
-
-casilla_resultado.innerHTML = "¡" + poke1.nombre + " atacó a " + poke2.nombre + " y " + poke2.nombre + " tiene " + poke2.vida + " de vida restante."+ "!";
-
-}
-
-
+	if (pokemon1 == pokemon2) {
+		alert("¡Los Pokemones no se pueden atacar a sí mismos!")
+	}else{
+		var poke1 = new Pokemon(pokemon1, + color + poderDeAtaque);
+		var poke2 = new Pokemon(pokemon2, + color + poderDeAtaque);
+		
+	}
+casilla_resultado.innerHTML = "¡" + poke1.nombre + " atacó a " + poke2.nombre + " y " + poke2.nombre + " tiene " + poke2.vida + " de vida restante!";
+};
 
 
 
